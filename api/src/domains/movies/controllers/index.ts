bb0716d8dba08f2e44dcb314e90651ddf1409283
GabrielLimab <galimabarros@gmail.com>
2024-06-23 17:27:35 -0300

    movie-request

--- api/src/domains/movies/controllers/index.ts
+++ api/src/domains/movies/controllers/index.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { MovieService } from '../services/MovieService';
-import { verifyJWT } from '../../../middlewares/auth';
+import { loginMiddleware,
+  verifyJWT,
+  notLoggedIn } from '../../../middlewares/auth';
 import { statusCodes } from '../../../../utils/constants/status-codes';
 export const router = Router();
 
@@ -22,6 +24,66 @@ router.get('/:id',
     try {
       const movieImages = await MovieService.getMovieDetails(req.params.id);
       res.status(statusCodes.SUCCESS).json(movieImages);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post('/:id/rating',
+  verifyJWT,
+  async(req: Request, res: Response, next:NextFunction) => {
+    try {
+      const rating = await MovieService.createRating(req.params.id, req.body.rating, req.userId!);
+      res.status(statusCodes.CREATED).json(rating);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.get('/:id/rating',
+  verifyJWT,
+  async(req: Request, res: Response, next:NextFunction) => {
+    try {
+      const rating = await MovieService.getRating(req.params.id);
+      res.status(statusCodes.SUCCESS).json(rating);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/:id/review',
+  verifyJWT,
+  async(req: Request, res: Response, next:NextFunction) => {
+    try {
+      const review = await MovieService.createReview(req.params.id, req.body.review, req.userId!);
+      res.status(statusCodes.CREATED).json(review);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.get('/:id/reviews',
+  verifyJWT,
+  async(req: Request, res: Response, next:NextFunction) => {
+    try {
+      const reviews = await MovieService.getReviews(req.params.id);
+      res.status(statusCodes.SUCCESS).json(reviews);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post('/:id/watched',
+  verifyJWT,
+  async(req: Request, res: Response, next:NextFunction) => {
+    try {
+      const watched = await MovieService.createWatched(req.params.id, req.userId!);
+      res.status(statusCodes.CREATED).json(watched);
     } catch (error) {
       next(error);
     }
